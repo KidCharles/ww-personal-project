d9170bf81f524cc742881d4558c994b33529f604
Charles Hatch <charlesghatch@gmail.com>
2018-06-25 09:21:00 -0600

    added video

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../Assets/ww_logo_white.svg';
 import background from '../Assets/login_background.svg';
+import video from '../Assets/ww_vid_compressed.mp4'
 import './Login.css';
 
 export default class Login extends Component {
@@ -14,6 +15,11 @@ export default class Login extends Component {
     render() {
         return (
             <div className='login'  >
+                <div>
+                <video className="video" autoPlay="true" loop='true' >
+                    <source src={ video } type="video/mp4" />
+                </video>
+                </div>
                 <img src={background} className='background' alt='textured' />
                 <img src={logo} className='logo' alt='Wayfaring World logo' />
                 <div className='loginInputs column' >
